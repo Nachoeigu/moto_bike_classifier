35eacd1efee92bb8d202a0343bb6f609df3f8fb0
Nacho Eigu <ignacio.eiguren@gmail.com>
2022-08-19 15:59:00 -0300

    Create constants.py

---
+++ constants.py
@@ -0,0 +1,58 @@
+MOTORCYCLE_SUBCATEGORIES = [
+    'agua', 
+    'amarillo', 
+    'azul', 
+    'azul-acero', 
+    'beige', 
+    'blanco',
+    'bordo', 
+    'celeste',
+    'coral-claro', 
+    'dorado', 
+    'gris', 
+    'gris-oscuro', 
+    'lavanda', 
+    'marron', 
+    'naranja', 
+    'negro', 
+    'plateado', 
+    'rojo', 
+    'verde', 
+    'violeta'
+]
+
+BIKE_SUBCATEGORIES = [
+        '/deportes-fitness/ciclismo/bicicletas/rodado-29/',
+        '/deportes-fitness/ciclismo/bicicletas/rodado-28/',
+        '/deportes-fitness/ciclismo/bicicletas/rodado-27.5/',
+        '/deportes-fitness/ciclismo/bicicletas/rodado-27/',
+        '/deportes-fitness/ciclismo/bicicletas/rodado-26/',
+        '/deportes-fitness/ciclismo/bicicletas/rodado-24/',
+        '/deportes-fitness/ciclismo/bicicletas/rodado-22/',
+        '/deportes-fitness/ciclismo/bicicletas/rodado-21/',
+        '/deportes-fitness/ciclismo/bicicletas/rodado-20/',
+        '/deportes-fitness/ciclismo/bicicletas/rodado-18/',
+        '/deportes-fitness/ciclismo/bicicletas/rodado-16/',
+        '/deportes-fitness/ciclismo/bicicletas/rodado-15/',
+        '/deportes-fitness/ciclismo/bicicletas/rodado-14/',
+        '/deportes-fitness/ciclismo/bicicletas/rodado-12/'
+]
+
+BASIC_HEADER = {
+            'authority': 'listado.mercadolibre.com.ar',
+            'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+            'accept-language': 'en-US',
+            'referer': 'https://listado.mercadolibre.com.ar/',
+            'sec-fetch-dest': 'document',
+            'sec-fetch-mode': 'navigate',
+            'sec-fetch-site': 'same-origin',
+            'sec-fetch-user': '?1',
+            'sec-gpc': '1',
+            'upgrade-insecure-requests': '1',
+            'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.5005.61 Safari/537.36',
+        }
+
+TRAIN_DIRECTORY = "data/train"
+
+TEST_DIRECTORY = "data/test"
+
